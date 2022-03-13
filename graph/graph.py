0c58d32cfffcc2faf6adef18bf92f19a437e50cc
SirJohnDoh <john.h.forsberg@gmail.com>
2022-03-13 12:15:17 +0100

    Adds graph module. Adds tests for graph module. Adds tox configuration. Adds __pycache__ to gitignore.

---
+++ graph/graph.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from functools import total_ordering
+from math import sqrt
+from typing import Callable
+
+
+def eculidean_distance(v1, v2):
+    x2 = pow(v2.x - v1.x, 2)
+    y2 = pow(v2.y - v1.y, 2)
+    return sqrt(x2 + y2)
+
+
+class Vertex:
+    def __init__(self, x, y, distance_function: Callable[[Vertex, Vertex], float] = eculidean_distance):
+        self.x = x
+        self.y = y
+        self.distance_function = distance_function
+
+    def distance_to(self, other: Vertex):
+        return self.distance_function(self, other)
+
+    def __eq__(self, other):
+        return other and self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash(self.x) + 37 * hash(self.y)
+
+    def __getitem__(self, key):
+        if (key == 0):
+            return self.x
+        return self.y
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"({self.x:.2f},{self.y:.2f})"
+
+
+@total_ordering
+class Edge:
+    def __init__(self, v1: Vertex, v2: Vertex):
+        self.v1 = v1
+        self.v2 = v2
+
+    def distance(self):
+        return self.v1.distance_to(self.v2)
+
+    def __lt__(self, other: Edge):
+        return self.distance() < other.distance()
+
+    def __eq__(self, other: Edge):
+        return (
+            self.distance() == other.distance() and
+            self.v1 == other.v1 and
+            self.v2 == other.v2
+        )
---
+++ tests/test_graph.py
@@ -0,0 +1,53 @@
+import pytest
+
+from math import sqrt
+
+from graph.graph import Vertex, Edge
+
+
+@pytest.mark.parametrize(
+    'v1,v2,expected_distance',
+    [
+        (Vertex(0, 0), Vertex(1, 0), 1.0),
+        (Vertex(0, 0), Vertex(0, 1), 1.0),
+        (Vertex(0, 0), Vertex(1, 1), sqrt(2)),
+    ]
+)
+def test_vertex_distance(v1, v2, expected_distance):
+    assert v1.distance_to(v2) == v2.distance_to(v1) == expected_distance
+
+
+@pytest.mark.parametrize(
+    'edge,expected_distance',
+    [
+        (Edge(Vertex(0, 0), Vertex(1, 0)), 1.0),
+        (Edge(Vertex(0, 0), Vertex(0, 1)), 1.0),
+        (Edge(Vertex(0, 0), Vertex(1, 1)), sqrt(2)),
+    ]
+)
+def test_edge_distance(edge, expected_distance):
+    assert edge.distance() == expected_distance
+
+
+@pytest.mark.parametrize(
+    'edges,expected_order',
+    [
+        (
+            [
+                Edge(Vertex(0, 0), Vertex(1, 1)),
+                Edge(Vertex(0, 0), Vertex(1, 0)),
+                Edge(Vertex(0, 0), Vertex(0, 1)),
+                Edge(Vertex(0, 0), Vertex(0, 0)),
+            ],
+            [
+                Edge(Vertex(0, 0), Vertex(0, 0)),
+                # Stable sort, this should be after the one with same distance
+                Edge(Vertex(0, 0), Vertex(1, 0)),
+                Edge(Vertex(0, 0), Vertex(0, 1)),
+                Edge(Vertex(0, 0), Vertex(1, 1)),
+            ],
+        ),
+    ]
+)
+def test_order_edges(edges, expected_order):
+    assert sorted(edges) == expected_order
