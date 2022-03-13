516bda90675256295f008e839348ad4b7d9b0b0a
SirJohnDoh <john.h.forsberg@gmail.com>
2022-03-13 15:46:42 +0100

    Adds demonstration program to run either minimum spanning tree or dreyfus wagner.

---
+++ tree_spanner.py
@@ -0,0 +1,155 @@
+#!env/bin/python
+
+import argparse
+import random
+
+from algorithms.dreyfus_wagner import DreyfusWagnerAlgorithm
+from algorithms.minimum_spanning_tree import MinimumSpanningTree
+from graph.graph import Vertex, eculidean_distance
+
+
+def pick_algorithm(algorithm):
+    if algorithm == 'dfw':
+        return DreyfusWagnerAlgorithm
+    if algorithm == 'mst':
+        return MinimumSpanningTree
+    raise Exception(f'Unknown algorithm {algorithm}')
+
+
+def pick_distance_function(distance_function):
+    if distance_function == 'euclidian':
+        return eculidean_distance
+    raise Exception(f'Unknown distance function {distance_function}')
+
+
+def randomize_terms_and_vertices(
+    terminal_count,
+    verticies_count,
+    xmin,
+    xmax,
+    ymin,
+    ymax,
+):
+    def rand_vert():
+        return Vertex(random.uniform(xmin, xmax), random.uniform(ymin, ymax))
+
+    terminals = [
+        rand_vert() for i in range(terminal_count)
+    ]
+    vertices = [
+        rand_vert() for i in range(terminal_count)
+    ]
+    return terminals, vertices
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-a', '---algorithm',
+        default='mst',
+        help='The algorithm to run Dreyfus Wagner (dwf), Minimum Spanning tree (mst). Default mst',
+        choices=['dfw', 'mst'],
+        type=str
+    )
+    parser.add_argument(
+        '-d', '--distance_function',
+        help='Function to calculate distance between vertices.',
+        default='euclidian',
+        choices=['euclidian'],
+        type=str,
+    )
+    parser.add_argument(
+        '-t', '--terminals',
+        help='List of terminal vertices in the form of x1,y1 x2,y2 ... . For example 1.0,1.0 2.0,2.0',
+        type=Vertex.from_str,
+        nargs='+',
+        default=[],
+    )
+    parser.add_argument(
+        '-v', '--vertices',
+        help='List of optional vertices, same format as terminals. Only used if the algorithm is Dreyfus Wagner.',
+        type=Vertex.from_str,
+        nargs='+',
+        default=[],
+    )
+    parser.add_argument(
+        '-r', '--random',
+        help='Randomizes input terminals and optional verticies',
+        action='store_true',
+        default=False,
+    )
+    parser.add_argument(
+        '--tcount',
+        help='Number of terminals to randomize, use with -r',
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        '--vcount',
+        help='Number of optional verticies to randomize, use with -r',
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        '--xmin',
+        help='Minimum value of x-axis to randomize, use with -r',
+        default=0.0,
+    )
+    parser.add_argument(
+        '--xmax',
+        help='Maximum value of x-axis to randomize, use with -r',
+        default=10.0,
+    )
+    parser.add_argument(
+        '--ymin',
+        help='Minimum value of y-axis to randomize, use with -r',
+        default=0.0,
+    )
+    parser.add_argument(
+        '--ymax',
+        help='Maximum value of y-axis to randomize, use with -r',
+        default=10.0,
+    )
+    parser.add_argument(
+        '--seed',
+        help='Random seed to use',
+        type=int,
+        required=False,
+    )
+    arguments = parser.parse_args()
+    arguments.algorithm = pick_algorithm(arguments.algorithm)
+    arguments.distance_function = pick_distance_function(arguments.distance_function)
+
+    if arguments.random:
+        if arguments.seed:
+            random.seed(arguments.seed)
+
+        arguments.terminals, arguments.vertices = randomize_terms_and_vertices(
+            arguments.tcount,
+            arguments.vcount,
+            arguments.xmin,
+            arguments.xmax,
+            arguments.ymin,
+            arguments.ymax,
+        )
+
+    # Setup the distance function of the verticis
+    for vert in arguments.terminals + arguments.vertices:
+        vert.distance_function = arguments.distance_function
+
+    return arguments
+
+
+if __name__ == '__main__':
+    arguments = parse_arguments()
+
+    print(f'Tree to span has {len(arguments.terminals)} terminal(s) and {len(arguments.vertices)} optional node(s).')
+    print(f'Solution is searched with: {arguments.algorithm.__name__}')
+
+    edges, total_cost = arguments.algorithm(arguments.terminals, arguments.vertices).solve()
+
+    print('Edges:')
+    for e in edges:
+        print(f'\t{e}')
+
+    print(f'Total edge length: {total_cost:.2f}')
